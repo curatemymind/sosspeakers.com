960a833f0064592ff8f4f8ca891611f5f27a8d96
Orlando Kenny <kenny@cs.fsu.edu>
2020-08-17 14:26:32 -0400

    trying net forms

--- src/pages/minor/ContactForm.js
+++ src/pages/minor/ContactForm.js
@@ -2,62 +2,80 @@ import React from 'react';
 import '../../App.css'
 import * as Util from './Util'
 
-const encode = (data) => {
+function encode(data) {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
-class ContactForm extends React.Component { 
-
-  //the states of emotion and source will be set to null initially until the user had filled out the form.
+export default class Contact extends React.Component {
   constructor(props) {
     super(props);
-      this.state = { name: "", email: "", message: "" };
+    this.state = {};
   }
 
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
     })
-      .then(() => alert("Success!"))
+      .then(() => alert("nice!"))
       .catch(error => alert(error));
-
-    e.preventDefault();
   };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-
-
- 
   render() {
-      const { name, email, message } = this.state;
-      return (
-        <form onSubmit={this.handleSubmit} method="POST" data-netlify="true" netlify>
-          <p>
+    return (
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
             <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
             </label>
           </p>
           <p>
             <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
             </label>
           </p>
           <p>
             <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
             </label>
           </p>
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
-      );
-    }
+      </div>
+    );
+  }
 }
-export default ContactForm;
-
