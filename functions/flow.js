456ac68a93a1ed84a771d9c0107cc01cfad95a22
Orlando Kenny <kenny@cs.fsu.edu>
2020-08-20 20:41:39 -0400

    net func session id

---
+++ functions/flow.js
@@ -0,0 +1,25 @@
+const stripe = require('stripe')('sk_test_51HDDkWEhI2c5jgt0V2LrkkrzvR5Q0DaNjK2l0l0YRI0mfS0M7mOmeMYO4dTsw313jZQDiszQ8HCbCuNUNb4Y3Wuc00Va204Y4I');
+
+
+exports.handler = async ( event, context) => { 
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'T-shirt',
+        },
+        unit_amount: 2000,
+      },
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'https://example.com/cancel',
+  });
+
+
+  return { statusCode : 200 , body : JSON.stringify({sessionId: session.id})};
+ }
