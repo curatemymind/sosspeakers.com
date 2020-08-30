a512be4ae3be7c1d6db304733a74d7ce6188f129
Orlando Kenny <kenny@cs.fsu.edu>
2020-08-30 11:04:17 -0400

    speakers ui

--- functions/allProducts.js
+++ functions/allProducts.js
@@ -2,8 +2,8 @@ const stripe = require('stripe')(process.env.REACT_APP_SANDBOX_SECRET_KEY);
 
 
 exports.handler = async ( event, context) => { 
-    const products = await stripe.products.list()
-    const pricesList = await stripe.prices.list()
+    const products = await stripe.products.list({limit: 1000})
+    const pricesList = await stripe.prices.list({limit: 1000})
 
     var prices = []
     for(var i = 0; i < (pricesList.data).length; i++)
@@ -13,24 +13,37 @@ exports.handler = async ( event, context) => {
             prices.push({[pricesList.data[i].product]: pricesList.data[i].id, USD: pricesList.data[i]['unit_amount']/100})
         }
     }
-    console.log(prices)
+
     var response = []
     var innerIndex = 0;
+    var activeProducts = []
 
 
     for(var i = 0; i < (products.data).length; i++)
     { 
         //console.log(products.data[i].metadata.DISCOUNT)
         //console.log(products.data[i].active)
-        if(products.data[i].active === true)
+        if(products.data[i].active)
         {
-            if((i === 0) || (products.data[i].name != products.data[i-1].name))
+            activeProducts.push(products.data[i])
+        }
+       
+        
+    }
+
+    for(var i = 0; i < (activeProducts).length; i++)
+    { 
+        //console.log(activeProducts[i].name)
+        //console.log(products.data[i].metadata.DISCOUNT)
+        //console.log(products.data[i].active)
+        
+            if((i === 0) || (activeProducts[i].name != activeProducts[i-1].name))
             {
                 for(var j = 0; j < prices.length; j++)
                 {
-                    if(prices[j][products.data[i].id])
+                    if(prices[j][activeProducts[i].id])
                     {
-                        response.push({NAME: products.data[i].name, DESCRIPTION: products.data[i].description, PHOTO: products.data[i].images[0], DISCOUNT: products.data[i].metadata.DISCOUNT, LINKS: [{[products.data[i].metadata.TYPE]: [products.data[i].id, prices[j][products.data[i].id],  prices[j].USD]}]})
+                        response.push({NAME: activeProducts[i].name, DESCRIPTION: activeProducts[i].description, PHOTO: activeProducts[i].images[0], DISCOUNT: activeProducts[i].metadata.DISCOUNT, LINKS: [{[activeProducts[i].metadata.TYPE]: [activeProducts[i].id, prices[j][activeProducts[i].id],  prices[j].USD]}]})
                     }
                 } 
             }
@@ -38,19 +51,22 @@ exports.handler = async ( event, context) => {
             {  
                 for(var j = 0; j < prices.length; j++)
                 {
-                    if(prices[j][products.data[i].id])
+                    if(prices[j][activeProducts[i].id])
                     {
-                        response[innerIndex].LINKS.push({[products.data[i].metadata.TYPE]: [products.data[i].id, prices[j][products.data[i].id], prices[j].USD]})  
+                        response[innerIndex].LINKS.push({[activeProducts[i].metadata.TYPE]: [activeProducts[i].id, prices[j][activeProducts[i].id], prices[j].USD]})  
+                        //response[innerIndex].LINKS.push({[products.data[i].metadata.TYPE]: [products.data[i].id, prices[j][products.data[i].id], prices[j].USD]})  
                     }
                 } 
-                
             }
-            if((i !== 0) && (products.data[i].name != products.data[i-1].name))
+            if((i !== 0) && (activeProducts[i].name != activeProducts[i-1].name))
             {
+                //issue is with innerindex
                 innerIndex++
+                
             }   
-        }
+        
+    
     }
-    console.log(response)
+    
     return { statusCode : 200 , body : JSON.stringify(response)};
  }
